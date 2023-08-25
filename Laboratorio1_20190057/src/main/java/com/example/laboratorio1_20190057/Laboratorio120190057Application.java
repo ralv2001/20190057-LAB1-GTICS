87ecd5bea15d9e1107bf601ca10f33890c1bc97a
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-08-24 22:01:46 -0500

    a

---
+++ Laboratorio1_20190057/src/main/java/com/example/laboratorio1_20190057/Laboratorio120190057Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio1_20190057;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio120190057Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio120190057Application.class, args);
+    }
+
+}
